70052ee4f3df4bc49655d02e2e045d09458bc456
Jouni Järvinen <rautamiekka@users.noreply.github.com>
2018-03-04 21:09:38 +0200

    Added a warning.

--- Convert every FLAC to 320kb MP3 CBR.bat
+++ Convert every FLAC to 320kb MP3 CBR.bat
@@ -1,3 +1,5 @@
+REM REM NOTE: This'll auto-delete the FLAC after FFmpeg terminates.
+
 FOR /R "%~dp0" %%f IN (*.flac) DO (
     "C:\Users\Public\Downloads\ffmpeg\bin\ffmpeg" -i "%%f" -codec:a libmp3lame -b:a 320k -compression_level 0 "%%~nf".mp3
     DEL /F "%%f"
