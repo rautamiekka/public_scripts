92adc712dea73ac9036ed6d74b563f9e25f92870
Jouni Järvinen <rautamiekka@users.noreply.github.com>
2020-11-07 22:13:46 +0200

    Final update
    SAV Free for Linux is dead.

--- sophos_anti-virus_for_linux.sh
+++ sophos_anti-virus_for_linux.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env sh
+{
+    echo "SAV Free for Linux is dead for 4 months by now (2020-11-07). Those Micro\$oft capitalist fanboys killed another product that had **some** use for Linux so that they can get more money to give M\$. All other free Linux antiviruses that I've found are worthless. Don't even get me started on ClamAV."
+    echo 'If you wanna use this script you need to remove this notice from the script.'
+    echo "It was fun while it lasted. I were writing more code when I found out the software's dead."
+    echo 'See you in Hell, Sophos. --rautamiekka, 2020-11-07'
+    return
+}
+
 ##Tested 2019-12-09 on Xubuntu 18.04.3 LTS x64, this script prepares the system for the full installation of Sophos Anti-Virus for Linux.
 ##The way the installation is made, some of the installation options will need to be given during the main installer.
 
