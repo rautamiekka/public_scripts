5d8bd432487e7931a034421f21aa2451dbf2335a
Jouni Järvinen <rautamiekka@users.noreply.github.com>
2023-10-03 22:45:28 +0300

    Update and rename global_shell_aliases to global_shell_aliases.sh
    - Fix the missed reference to the customizable variable for the 7-Zip binary name.
    - Add a customizable variable for the thread count for 7-Zip & XZ for portability, so the commands themselves don't need editing.
    - Improve the comments.

--- global_shell_aliases.sh
+++ global_shell_aliases.sh
@@ -7,62 +7,66 @@
 ##  As of Ubuntu 22.04 LTS the package is called `7zip` in which the binary is called `7zz`. The old package `p7zip[-full]` in which it's called `7z` was abandoned for ages and not updated beyond 16.02. The one for Ubuntu 22.04 LTS is still behind Ubuntu 22.10, but still better than the ancient 7-Zip 16.02.
 _binary_7z='7zz'
 
+##Change this according to how many threads you want the 7-Zip and XZ binaries to use for compression.
+##  REMEMBER TO TEST THE MEMORY USAGE BEFOREHAND !
+_threads_7z_xz=3
+
 for _mb in 64 128 192 256 384 512 768 1000 1536; do
     ##Enable only if `7z` is somehow installed in `$PATH`.
-    ##  Compress using P7-Zip with LZMA2 and ${_mb}MB dictionary size. 64MB still works on 32-bit software and system.
-    ##    Uses lowest possible non-idle priorities to impose as little performance penalty as possible on the other processes, _
-    ##      which in turn makes the compression slower.
-    if [ "$(command -v 7z)" ]; then
+    ##  Compress using P7-Zip with LZMA2, ${_mb}MB dictionary size, and ${_threads_7z_xz} threads. 64MB still works on 32-bit software and system.
+    ##  Uses lowest possible non-idle priorities to impose as little performance penalty as possible on the other processes, _
+    ##    which in turn makes the compression slower.
+    [ "$(command -v "${_binary_7z}")" ] && {
         # shellcheck disable=SC2139  #We know the var expands during definition of the alias.
-        alias 7zfull${_mb}='echo '\''NOTE: §7zfull'"${_mb}"'§ stores the target data of a symlink instead of the symlink itself.'\''; ionice -c 2 -n 7 nice -n 20 '"${_binary_7z}"' a -t7z -mx=9 -ms=on -mhc=on -mhe=off -m0=LZMA2:a=1:d='"${_mb}"'m:mf=bt4:fb=273:mc=1000000000:lc=3:lp=1:pb=4 -mmt=1 -mtc=off -bb3 -bt -ssw '
+        alias 7zfull${_mb}='echo '\''NOTE: §7zfull'"${_mb}"'§ stores the target data of a symlink instead of the symlink itself.'\''; ionice -c 2 -n 7 nice -n 20 '"${_binary_7z}"' a -t7z -mx=9 -ms=on -mhc=on -mhe=off -m0=LZMA2:a=1:d='"${_mb}"'m:mf=bt4:fb=273:mc=1000000000:lc=3:lp=1:pb=4 -mmt='"${_threads_7z_xz}"' -mtc=off -bb3 -bt -ssw '
 
         # shellcheck disable=SC2139  #We know the var expands during the definition of the alias.
-        alias 7zfullsym${_mb}='echo '\''NOTE: §7zfullsym'"${_mb}"'§ stores symlinks as is instead of the target data.'\''; ionice -c 2 -n 7 nice -n 20 '"${_binary_7z}"' a -t7z -mx=9 -ms=on -mhc=on -mhe=off -m0=LZMA2:a=1:d='"${_mb}"'m:mf=bt4:fb=273:mc=1000000000:lc=3:lp=1:pb=4 -mmt=1 -mtc=off -bb3 -bt -ssw '
-    fi
+        alias 7zfullsym${_mb}='echo '\''NOTE: §7zfullsym'"${_mb}"'§ stores symlinks as is instead of the target data.'\''; ionice -c 2 -n 7 nice -n 20 '"${_binary_7z}"' a -t7z -mx=9 -ms=on -mhc=on -mhe=off -m0=LZMA2:a=1:d='"${_mb}"'m:mf=bt4:fb=273:mc=1000000000:lc=3:lp=1:pb=4 -mmt='"${_threads_7z_xz}"' -mtc=off -bb3 -bt -ssw '
+    }
 
     ##Enable only if `xz` is somehow installed in `$PATH`.
-    ##  Compress using XZ with LZMA2 and ${_mb}MB dictionary size. 64MB still works on 32-bit software and system.
-    ##    Uses lowest possible non-idle priorities to impose as little performance penalty as possible on the other processes, _
-    ##      which in turn makes the compression slower.
-    if [ "$(command -v xz)" ]; then
+    ##  Compress using XZ with LZMA2, ${_mb}MB dictionary size, and ${_threads_7z_xz} threads, in Extreme mode. 64MB still works on 32-bit software and system.
+    ##  Uses lowest possible non-idle priorities to impose as little performance penalty as possible on the other processes, _
+    ##    which in turn makes the compression slower.
+    [ "$(command -v 'xz')" ] && {
         # shellcheck disable=SC2139  #We know the var expands during the definition of the alias.
-        alias xzfull${_mb}='ionice -c 2 -n 7 nice -n 20 xz -vvz9e --lzma2=dict='"${_mb}"'MiB,mf=bt4,nice=273 --threads=3 '
-    fi
+        alias xzfull${_mb}='ionice -c 2 -n 7 nice -n 20 xz -vvz9e --lzma2=dict='"${_mb}"'MiB,mf=bt4,nice=273 --threads='"${_threads_7z_xz}"' '
+    }
 done
 
-for _app in aptitude apt apt-get; do
-    if [ "$(command -v ${_app})" ]; then
+for _app in 'aptitude' 'apt' 'apt-get'; do
+    [ "$(command -v "${_app}")" ] && {
         ##Update package listing and show updatable packages, only creating an alias matching the app in the given order.
         ##Uses lowest possible non-idle priorities to impose as little performance penalty as possible on the other processes, _
         ##  which in turn potentially makes the upgrade process slower since every process spawned by ${_app} will inherit its priorities.
         # shellcheck disable=SC2139  #We know the var expands during the definition of the alias.
         alias upg='sudo '"${_app}"' update; sudo '"${_app}"' full-upgrade ' && break
-    fi
+    }
 done
 
-if [ "$(command -v ethstatus)" ]; then
+[ "$(command -v 'ethstatus')" ] && {
     ##Make `ethstatus` have the lowest possible non-idle priorities.
     alias ethstatus='sudo ionice -c 2 -n 7 nice -n 20 ethstatus '
 
     ##Optional: same as above, but for first WLAN.
     #alias ethstatus_wlan0='sudo ionice -c 2 -n 7 nice -n 20 ethstatus -i wlan0 '
-fi
+}
 
-if [ "$(command -v iotop)" ]; then
+[ "$(command -v 'iotop')" ] && {
     ##Make `iotop` have the lowest possible non-idle priorities and show only the processes which are doing IO.
     alias iotop='sudo ionice -c 2 -n 7 nice -n 20 iotop -o '
-fi
+}
 
-if [ "$(command -v dpkg)" ]; then
+[ "$(command -v 'dpkg')" ] && {
     ##If it doesn't print anything but the standard warning about GNOME Virtual File System, it's safe to manually delete the lock.
     alias print_dpkg_lock_holders='sudo lsof | grep '\''/var/lib/dpkg/lock'\'' '
-fi
+}
 
 ####Make both `htop`, `top`, and any other programs have the lowest possible non-idle priorities.####
 for _app in htop top btop; do
-    if [ "$(command -v ${_app})" ]; then
+    [ "$(command -v "${_app}")" ] && {
         # shellcheck disable=SC2139  #We know the var expands during the definition of the alias.
         alias ${_app}='sudo ionice -c 2 -n 7 nice -n 20 '"${_app}"' '
-    fi
+    }
 done
 ################################################################################
